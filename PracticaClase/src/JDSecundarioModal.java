9db1cd7cd98e1bf49c12474b6107eba38857b4dd
lilwanan <jba0018@alu.medac.es>
2024-10-07 14:40:53 +0200

    añado

---
+++ PracticaClase/src/JDSecundarioModal.java
@@ -0,0 +1,50 @@
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+
+public class JDSecundarioModal extends JDialog {
+
+	private static final long serialVersionUID = 1L;
+	private final JPanel contentPanel = new JPanel();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			JDSecundarioModal dialog = new JDSecundarioModal();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public JDSecundarioModal() {
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(null);
+		contentPanel.setBounds(0, 0, 434, 228);
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel);
+		contentPanel.setLayout(null);
+		{
+			JLabel lbetiquetamodal = new JLabel("estoy en un modal");
+			lbetiquetamodal.setBounds(75, 97, 181, 90);
+			contentPanel.add(lbetiquetamodal);
+		}
+	}
+
+	public JDSecundarioModal(ActionListener actionListener, boolean b) {
+		super();
+	}
+
+}
